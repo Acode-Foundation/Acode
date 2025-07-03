ac1651eb5f371227821aff28985f74e7649f74ff
RohitKushvaha01 <oldisg131@gmail.com>
2025-07-03 18:03:14 +0530

    feat. handle fdroid flavor

--- src/plugins/terminal/scripts/init-sandbox.sh
+++ src/plugins/terminal/scripts/init-sandbox.sh
@@ -1,25 +1,26 @@
 export LD_LIBRARY_PATH=$PREFIX
 export PROOT_TMP_DIR=$PREFIX/tmp
-export PROOT_LOADER=$NATIVE_DIR/libproot.so
+
+
+if [ -f "$NATIVE_DIR/libproot.so" ]; then
+    export PROOT_LOADER="$NATIVE_DIR/libproot.so"
+fi
 
 if [ -f "$NATIVE_DIR/libproot32.so" ]; then
     export PROOT_LOADER32="$NATIVE_DIR/libproot32.so"
 fi
 
-
 mkdir -p "$PREFIX/tmp"
-
-if [ -e "$PREFIX/libtalloc.so.2" ] || [ -L "$PREFIX/libtalloc.so.2" ]; then
-    rm "$PREFIX/libtalloc.so.2"
-fi
-
-ln -s "$NATIVE_DIR/libtalloc.so" "$PREFIX/libtalloc.so.2"
 
 if [ "$FDROID" = "true" ]; then
     export PROOT="$PREFIX/libproot-xed.so"
     chmod +x $PROOT
     chmod +x $PREFIX/libtalloc.so.2
 else
+    if [ -e "$PREFIX/libtalloc.so.2" ] || [ -L "$PREFIX/libtalloc.so.2" ]; then
+        rm "$PREFIX/libtalloc.so.2"
+    fi
+    ln -s "$NATIVE_DIR/libtalloc.so" "$PREFIX/libtalloc.so.2"
     export PROOT="$NATIVE_DIR/libproot-xed.so"
 fi
 
