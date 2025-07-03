1c4c754f027bfef3d6b53532e452159646b27bc8
RohitKushvaha01 <oldisg131@gmail.com>
2025-07-03 19:01:30 +0530

    format

--- src/plugins/terminal/scripts/init-alpine.sh
+++ src/plugins/terminal/scripts/init-alpine.sh
@@ -1,9 +1,13 @@
 echo "$$" > $PREFIX/pid
 set -e  # Exit immediately on Failure
 
+mkdir -p /public
+
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/share/bin:/usr/share/sbin:/usr/local/bin:/usr/local/sbin:/system/bin:/system/xbin:$PREFIX/local/bin
 export PS1="\[\e[38;5;46m\]\u\[\033[39m\]@localhost \[\033[39m\]\w \[\033[0m\]\\$ "
 export PIP_BREAK_SYSTEM_PACKAGES=1
+export HOME=/public
+export TERM=xterm-256color
 required_packages="bash"
 
 missing_packages=""
@@ -35,6 +39,7 @@ if [ "$1" = "--installing" ]; then
   echo "Installation completed."
   exit 0
 fi
+
 
 
 if [ "$#" -eq 0 ]; then
