a0f9691ecb156cb36e8c1ab9b6cd8bd50e05fe81
RohitKushvaha01 <oldisg131@gmail.com>
2025-08-01 09:19:32 +0530

    .

--- src/lib/lspClient.js
+++ src/lib/lspClient.js
@@ -1,5 +1,5 @@
 export default class lspClient {
-    constructor({ serverUrl }) {
+    constructor(serverUrl) {
         this.serverUrl = serverUrl;
         this.ws = null;
         this.messageId = 0;
