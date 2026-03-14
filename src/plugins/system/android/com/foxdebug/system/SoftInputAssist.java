eb5ff177b218620ed00e3d82b046b58a5ecee32f
RohitKushvaha01 <oldisg131@gmail.com>
2026-03-14 14:24:49 +0530

    feat: added support for keyboards with no animations

--- src/plugins/system/android/com/foxdebug/system/SoftInputAssist.java
+++ src/plugins/system/android/com/foxdebug/system/SoftInputAssist.java
@@ -2,6 +2,7 @@ package com.foxdebug.system;
 
 import android.app.Activity;
 import android.view.View;
+import java.util.List;
 
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,21 +11,48 @@ import androidx.core.view.WindowInsetsAnimationCompat;
 
 public class SoftInputAssist {
 
+    private boolean animationRunning = false;
+
     public SoftInputAssist(Activity activity) {
         View contentView = activity.findViewById(android.R.id.content);
+
+        ViewCompat.setOnApplyWindowInsetsListener(contentView, (v, insets) -> {
+
+            if (!animationRunning) {
+                Insets ime = insets.getInsets(WindowInsetsCompat.Type.ime());
+                Insets nav = insets.getInsets(WindowInsetsCompat.Type.navigationBars());
+
+                int keyboardHeight = Math.max(0, ime.bottom - nav.bottom);
+
+                v.setPadding(0, 0, 0, keyboardHeight);
+            }
+
+            return insets;
+        });
 
         ViewCompat.setWindowInsetsAnimationCallback(
             contentView,
             new WindowInsetsAnimationCompat.Callback(
                 WindowInsetsAnimationCompat.Callback.DISPATCH_MODE_CONTINUE_ON_SUBTREE
             ) {
+
+                @Override
+                public void onPrepare(WindowInsetsAnimationCompat animation) {
+                    animationRunning = true;
+                }
+
+                @Override
+                public void onEnd(WindowInsetsAnimationCompat animation) {
+                    animationRunning = false;
+                }
+
                 @Override
                 public WindowInsetsCompat onProgress(
                         WindowInsetsCompat insets,
-                        java.util.List<WindowInsetsAnimationCompat> runningAnimations) {
+                        List<WindowInsetsAnimationCompat> runningAnimations) {
 
                     Insets ime = insets.getInsets(WindowInsetsCompat.Type.ime());
-                    Insets nav = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    Insets nav = insets.getInsets(WindowInsetsCompat.Type.navigationBars());
 
                     int keyboardHeight = Math.max(0, ime.bottom - nav.bottom);
 
