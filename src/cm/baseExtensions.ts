44f77cd9ead941ca18fb8cdb23e786b6798ee547
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2026-06-04 15:50:25 +0530

    feat: tag auto rename

--- src/cm/baseExtensions.ts
+++ src/cm/baseExtensions.ts
@@ -21,6 +21,7 @@ import {
 	rectangularSelection,
 	tooltips,
 } from "@codemirror/view";
+import tagAutoRename from "./tagAutoRename";
 
 /**
  * Base extensions roughly matching the useful parts of CodeMirror's basicSetup
@@ -38,6 +39,7 @@ export default function createBaseExtensions(): Extension[] {
 		syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
 		bracketMatching(),
 		closeBrackets(),
+		tagAutoRename(),
 		rectangularSelection(),
 		crosshairCursor(),
 		highlightActiveLine(),
