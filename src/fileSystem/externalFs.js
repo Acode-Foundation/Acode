6da53aed14970b1a9986f5c864d9bf9ea4d66f39
Rohit Kushwaha <oldisg131@gmail.com>
2026-06-22 12:36:15 +0530

    format

--- src/fileSystem/externalFs.js
+++ src/fileSystem/externalFs.js
@@ -196,7 +196,7 @@ function createFs(url) {
 				const isJson = encoding === "json";
 				const charset = getEncodingName(isJson ? null : encoding);
 				const text = await externalFs.readAsText(url, charset);
-				const stripped = text.charCodeAt(0) === 0xFEFF ? text.slice(1) : text;
+				const stripped = text.charCodeAt(0) === 0xfeff ? text.slice(1) : text;
 				return isJson ? JSON.parse(stripped) : stripped;
 			}
 
